6dc4e3423250aafeb59c84811ba9fcf98c72e9b8
Maciej Tabiś <maciek.tabis@gmail.com>
2023-02-04 23:43:18 +0100

    Add Redux to project

--- src/components/CardForm/CardForm.js
+++ src/components/CardForm/CardForm.js
@@ -1,26 +1,27 @@
 import styles from './CardForm.module.scss';
 import Button from '../Button/Button';
 import TextInput from '../TextInput/TextInput';
-import { useState } from 'react';
+import {useState} from 'react';
+import {useDispatch} from 'react-redux';
 
 const CardForm = (props) => {
-  const [title, setTitle] = useState('');
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    props.action({ title: title }, props.columnId);
-    setTitle('');
-  };
+	const [title, setTitle] = useState('');
+	const dispatch = useDispatch();
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		dispatch({type: 'ADD_CARD', payload: {title, columnId: props.columnId}});
+	};
 
-  return (
-    <form className={styles.cardForm} onSubmit={handleSubmit}>
-      <TextInput
-        type='text'
-        value={title}
-        onChange={(e) => setTitle(e.target.value)}
-      />
-      <Button>Add card</Button>
-    </form>
-  );
+	return (
+		<form className={styles.cardForm} onSubmit={handleSubmit}>
+			<TextInput
+				type='text'
+				value={title}
+				onChange={(e) => setTitle(e.target.value)}
+			/>
+			<Button>Add card</Button>
+		</form>
+	);
 };
 
 export default CardForm;
---
+++ src/redux/store.js
@@ -0,0 +1,25 @@
+import {createStore} from 'redux';
+import initialState from './initialState';
+import shortid from 'shortid';
+const reducer = (state, action) => {
+	switch (action.type) {
+		case 'ADD_COLUMN':
+			return {
+				...state,
+				columns: [...state.columns, {...action.payload, id: shortid()}],
+			};
+		case 'ADD_CARD':
+			return {
+				...state,
+				cards: [...state.cards, {...action.payload, id: shortid()}],
+			};
+		default:
+			return state;
+	}
+};
+const store = createStore(
+	reducer,
+	initialState,
+	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+export default store;
