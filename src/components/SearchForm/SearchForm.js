c3197b7243a9ff7920f19bcc7d4797486ded6218
Maciej Tabiś <maciek.tabis@gmail.com>
2023-02-05 20:08:13 +0100

    SearchForm start works

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,16 +1,30 @@
 import styles from './SearchForm.module.scss';
 import TextInput from '../TextInput/TextInput';
 import Button from '../Button/Button';
+import {useDispatch} from 'react-redux';
+import {useState} from 'react';
 
-const SearchForm = () => {
-    return (
-        <form className={styles.searchForm}>
-            <TextInput placeholder="Search..." />
-            <Button>  
-                <span className="fa fa-search" />
-            </Button>
-        </form>
-    );
-  };
+const SearchForm = (props) => {
+	const [searchValue, setValue] = useState('');
+	const dispatch = useDispatch();
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		dispatch({type: 'UPDATE_SEARCHSTRING', payload: searchValue});
+
+		setValue('');
+	};
+	return (
+		<form className={styles.searchForm} onSubmit={handleSubmit}>
+			<TextInput
+				placeholder='Search...'
+				value={searchValue}
+				onChange={(e) => setValue(e.target.value)}
+			/>
+			<Button>
+				<span className='fa fa-search' />
+			</Button>
+		</form>
+	);
+};
 
 export default SearchForm;
