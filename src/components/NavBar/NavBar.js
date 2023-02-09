140eca1515d2fc44e9e3292a7dc587080c378ac9
Maciej Tabiś <maciek.tabis@gmail.com>
2023-02-09 14:11:15 +0100

    Change the nav

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,25 +1,53 @@
 import styles from './NavBar.module.scss';
 import Container from '../Container/Container';
-import { Link } from 'react-router-dom';
-
-
+import {Link} from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
 
 const NavBar = () => {
-  return (
-    <nav className={styles.navBar} >
-       <Container>
-        <div className={styles.navigation}>
-        <i className={styles.icon + " fa fa-tasks"} aria-hidden="true"><Link to="/" /></ i>
+	return (
+		<nav className={styles.navBar}>
+			<Container>
+				<div className={styles.navigation}>
+					<i className={styles.icon + ' fa fa-tasks'} aria-hidden='true'>
+						<Link to='/' />
+					</i>
 
-            <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/favorite">Favorite</Link></li>
-                <li><Link to="/about">About</Link></li>
-            </ul>
-            </div>
-        </Container>
-  </nav>);
-}
-
+					<ul>
+						<li>
+							<NavLink
+								className={({isActive}) =>
+									isActive ? styles.linkActive : undefined
+								}
+								to='/'
+							>
+								Home
+							</NavLink>
+						</li>
+						<li>
+							<NavLink
+								className={({isActive}) =>
+									isActive ? styles.linkActive : undefined
+								}
+								to='/favorite'
+							>
+								Favorite
+							</NavLink>
+						</li>
+						<li>
+							<NavLink
+								className={({isActive}) =>
+									isActive ? styles.linkActive : undefined
+								}
+								to='/about'
+							>
+								About
+							</NavLink>
+						</li>
+					</ul>
+				</div>
+			</Container>
+		</nav>
+	);
+};
 
 export default NavBar;
